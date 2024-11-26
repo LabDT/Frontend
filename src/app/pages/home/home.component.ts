f59d275d18b7aa618b5ec50eea1a33622da7b896
Zoty <leonardokronbauer2002@gmail.com>
2024-11-26 15:05:34 -0300

    feat: add search to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { LoginModalComponent } from "../../modals/login-modal/login-modal.component";
 import { RegisterModalComponent } from "../../modals/register-modal/register-modal.component";
+import { InputComponent } from "../../components/input/input.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, LoginModalComponent, RegisterModalComponent],
+  imports: [HeaderComponent, LoginModalComponent, RegisterModalComponent, InputComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
+  onSearch() {
+
+  }
 }
